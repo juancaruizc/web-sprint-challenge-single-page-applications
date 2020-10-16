23a3f6a64d899881b663886925cf71aacfb27592
juancaruizc <juancruizcruz@gmail.com>
2020-10-16 14:15:46 -0700

    Test

--- src/App.js
+++ src/App.js
@@ -1,11 +1,21 @@
 import React from "react";
+import { Route, Link } from "react-router-dom"
+import PizzaApp from "./PizzaApp";
+
+
 
 const App = () => {
   return (
     <>
+      <nav className="navbar">
+        <li><Link to="/">Home</Link></li>
+        <li><Link to="/pizza">Pizzas</Link></li>
+      </nav>
       <h1>Lambda Eats</h1>
       <p>You can remove this code and create your own header</p>
+      <Route path="/pizza" component={PizzaApp}/>
     </>
   );
 };
+
 export default App;
---
+++ src/PizzaApp.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const PizzaApp = () => {
+    return (
+        <div>
+            
+        </div>
+    )
+}
+
+export default PizzaApp
